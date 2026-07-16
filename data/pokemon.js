349e98e634cdee0aa0277678f029108ee1a85353
danielp96x <danueru1621@gmail.com>
2026-07-16 07:58:35 -0500

    Update pokemon.js

--- data/pokemon.js
+++ data/pokemon.js
@@ -1,1 +1,38 @@
-
+const pokemon = [
+  {
+    id: 1,
+    nombre: "Bulbasaur",
+    tipo: ["Planta", "Veneno"],
+    color: "Verde",
+    alas: false,
+    legendario: false,
+    imagen: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+  },
+  {
+    id: 4,
+    nombre: "Charmander",
+    tipo: ["Fuego"],
+    color: "Naranja",
+    alas: false,
+    legendario: false,
+    imagen: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+  },
+  {
+    id: 7,
+    nombre: "Squirtle",
+    tipo: ["Agua"],
+    color: "Azul",
+    alas: false,
+    legendario: false,
+    imagen: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
+  },
+  {
+    id: 25,
+    nombre: "Pikachu",
+    tipo: ["Eléctrico"],
+    color: "Amarillo",
+    alas: false,
+    legendario: false,
+    imagen: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+  }
+];
