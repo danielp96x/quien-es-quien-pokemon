b1341a890d3e612d578d32282b7325e1dff27d7d
danielp96x <danueru1621@gmail.com>
2026-07-16 08:27:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-alert("Script cargado correctamente");
-
 const tablero = document.getElementById("tablero");
 const botonNueva = document.getElementById("nuevaPartida");
 const estado = document.getElementById("estado");
+
+let pokemonSecreto = null;
 
 function mostrarPokemon() {
     tablero.innerHTML = "";
 
     pokemon.forEach((poke) => {
         tablero.innerHTML += `
-            <div class="carta">
+            <div class="carta" data-id="${poke.id}">
                 <img src="${poke.imagen}" alt="${poke.nombre}">
                 <h3>${poke.nombre}</h3>
             </div>
@@ -17,8 +17,13 @@ function mostrarPokemon() {
     });
 }
 
-botonNueva.addEventListener("click", () => {
-    estado.textContent = "🎉 ¡El botón funciona!";
-});
+function iniciarPartida() {
+    const indice = Math.floor(Math.random() * pokemon.length);
+    pokemonSecreto = pokemon[indice];
+
+    estado.textContent = "🎮 ¡Partida iniciada! Ya puedes hacer preguntas.";
+}
+
+botonNueva.addEventListener("click", iniciarPartida);
 
 mostrarPokemon();
