e581556f6bb5d7e5673d3f446d00e30ac34a2425
danielp96x <danueru1621@gmail.com>
2026-07-15 23:21:28 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const jugar = document.getElementById("jugar");
+
+jugar.addEventListener("click", iniciarJuego);
+
+function iniciarJuego(){
+
+    alert("¡Bienvenido a ¿Quién es Quién? Pokémon Deluxe!");
+
+}
