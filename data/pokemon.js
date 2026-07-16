470d0e4dfd63486a78e4dc4d10595822fb8c8ae3
danielp96x <danueru1621@gmail.com>
2026-07-16 08:04:47 -0500

    Update pokemon.js

--- data/pokemon.js
+++ data/pokemon.js
@@ -1,38 +1,12 @@
 const pokemon = [
-  {
-    id: 1,
-    nombre: "Bulbasaur",
-    tipo: ["Planta", "Veneno"],
-    color: "Verde",
-    alas: false,
-    legendario: false,
-    imagen: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-  },
-  {
-    id: 4,
-    nombre: "Charmander",
-    tipo: ["Fuego"],
-    color: "Naranja",
-    alas: false,
-    legendario: false,
-    imagen: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
-  },
-  {
-    id: 7,
-    nombre: "Squirtle",
-    tipo: ["Agua"],
-    color: "Azul",
-    alas: false,
-    legendario: false,
-    imagen: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
-  },
-  {
-    id: 25,
-    nombre: "Pikachu",
-    tipo: ["Eléctrico"],
-    color: "Amarillo",
-    alas: false,
-    legendario: false,
-    imagen: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-  }
+  {id:1,nombre:"Bulbasaur",tipo:["Planta","Veneno"],color:"Verde",alas:false,legendario:false,imagen:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"},
+  {id:2,nombre:"Ivysaur",tipo:["Planta","Veneno"],color:"Verde",alas:false,legendario:false,imagen:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"},
+  {id:3,nombre:"Venusaur",tipo:["Planta","Veneno"],color:"Verde",alas:false,legendario:false,imagen:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png"},
+  {id:4,nombre:"Charmander",tipo:["Fuego"],color:"Naranja",alas:false,legendario:false,imagen:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"},
+  {id:5,nombre:"Charmeleon",tipo:["Fuego"],color:"Rojo",alas:false,legendario:false,imagen:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png"},
+  {id:6,nombre:"Charizard",tipo:["Fuego","Volador"],color:"Naranja",alas:true,legendario:false,imagen:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"},
+  {id:7,nombre:"Squirtle",tipo:["Agua"],color:"Azul",alas:false,legendario:false,imagen:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"},
+  {id:8,nombre:"Wartortle",tipo:["Agua"],color:"Azul",alas:false,legendario:false,imagen:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png"},
+  {id:9,nombre:"Blastoise",tipo:["Agua"],color:"Azul",alas:false,legendario:false,imagen:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png"},
+  {id:25,nombre:"Pikachu",tipo:["Eléctrico"],color:"Amarillo",alas:false,legendario:false,imagen:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"}
 ];
